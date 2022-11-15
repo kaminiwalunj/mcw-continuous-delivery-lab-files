5b6e8b2e9e1d554d578a468da3bb3a95b1756dd6
kaminiwalunj <88807671+kaminiwalunj@users.noreply.github.com>
2022-11-15 17:42:32 +0000

    Create seed.ps1

---
+++ .github/workflows/seed.ps1
@@ -0,0 +1,16 @@
+          docker login ghcr.io -u kamini@spektrasystems.com -p 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
+          $studentprefix = "790341"
+          $githubAccount = "kaminiwalunj"
+          $resourcegroupName = "fabmedical-rg-" + $studentprefix
+          $cosmosDBName = "fabmedical-cdb-" + $studentprefix
+          # Fetch CosmosDB Mongo connection string
+          $mongodbConnectionString = `
+          $(az cosmosdb keys list `
+              --name $cosmosDBName `
+              --resource-group $resourcegroupName `
+              --type connection-strings `
+              --query 'connectionStrings[0].connectionString')
+          # Seed CosmosDB database
+          docker run -ti `
+           -e MONGODB_CONNECTION="$mongodbConnectionString" `
+               ghcr.io/$githubAccount/fabrikam-init:main
