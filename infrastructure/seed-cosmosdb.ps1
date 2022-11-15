e02fa32f3ecf743acc0181a0a0c916ab9936aa42
kaminiwalunj <88807671+kaminiwalunj@users.noreply.github.com>
2022-11-15 22:27:36 +0000

    Update seed-cosmosdb.ps1

--- infrastructure/seed-cosmosdb.ps1
+++ infrastructure/seed-cosmosdb.ps1
@@ -1,4 +1,4 @@
-#echo 'uvnl2pbv4sk4dd5kwenkcqu5pjobsojuw6fayvhg6ehyyruwjndq' | docker login ghcr.io -u kaminiwalunj --password-stdin
+#echo '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' | docker login ghcr.io -u kaminiwalunj --password-stdin
 
 $studentprefix = "790341"
 $githubAccount = "kaminiwalunj"
